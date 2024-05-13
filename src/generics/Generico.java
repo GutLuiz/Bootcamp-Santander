48a8337d69f81d2235b9e4ff416ea0c3a25ff52a
Luiz Gustavo <gutfoda@gmail.com>
2024-05-13 18:55:56 -0300

    Introdução sobre Generics

---
+++ src/generics/Generico.java
@@ -0,0 +1,29 @@
+package generics;
+
+public class Generico {
+    /*
+    - Um tipo generico é uma classe generica ou uma interface que é parametrizada em relação a tipos.
+    - O simbulo <> é chamado de "Diamondd" ou "dimondd operator" foi um recurso introduzido no java 7 e é usado no contexto de
+    tipos genericos em java, para inferir automaaticamente o tipo com base no contexto.
+    - Para utilizar uma classe generica, voce cria uma declaração de tipo generico alterando o codigo
+    - Isso introduz a variavel de tipo, T, que pode ser usada em qualquer lugar dentro da classe:
+
+    Exemplo:
+
+    public class Box<T>{
+        private T t;
+
+        public void set(T t) { this.t = t;}
+        public t get() { return = t;}
+    }
+
+    - Todas as ocorrencias de Object (ou qualquer outro tipo) sao substituidas por T.
+    - Uma variavel de tipo pode ser qualquer tipo nao primitivo que voce especificar: qualquer tipo de classe, qualquer tipo
+    de interface qualquer tipo de array ou ate mesmo outra variavel.
+    - Essa mesma tecnica pode ser aplicada para criar interfaces generica.
+
+
+
+
+    */
+}
