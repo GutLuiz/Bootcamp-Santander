f1fc90e8541d6ce095c2a99f21ca10aabf93dec1
Luiz Gustavo <gutfoda@gmail.com>
2024-05-15 18:26:57 -0300

    List introdução

---
+++ src/List/List.java
@@ -0,0 +1,14 @@
+package List;
+
+public class List {
+    /*
+        list:
+        - É uma coleção ordenada que permite a inclusão de elementos duplicados.
+        - É um dos tipos de coleção mais utilizados em java, e as classes de implementtação comuns são ArrayList e LinkedList
+        - Se assemelha a uma matriz com comprimento dinamico, permitindo adicionar ou remover elementos.
+        - A interface list fornece metodos uteis para adicionar elementos em posições especificas, remover ou substituir elementos
+        com base no indice e obter sublistas usando indices
+        - A classe Collections fornece algoritimos uteis para manupulção de list, como ordenção (sort), embaralhamento (shuffle)
+        reversão (reverse) e busca binaria (binarySearch)
+     */
+}
